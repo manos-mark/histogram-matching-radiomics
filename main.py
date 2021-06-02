348c950c077d7dba149afb1702cac05e86bb8acf
manos <manos-mark@hotmail.com>
2021-06-02 17:57:43 +0300

    create roi paths on the new dataset

--- main.py
+++ main.py
@@ -17,10 +17,11 @@ def main():
     dataset = feature_extractor.import_prepare_dataset(DATASET_PATH)
         
     # Execute batch processing to extract features
-    # feature_extractor.extract_features(dataset, FEATURES_OUTPUT_PATH)
+    feature_extractor.extract_features(dataset, FEATURES_OUTPUT_PATH)
 
     # Get the filepaths from the images only (without the segmentations)
-    dataset = [value['Image'] for value in dataset.values()]
+    dataset_images = [value['Image'] for value in dataset.values()]
+
 
     ######################################################################################################
     ######################################### HISTOGRAM MATCHING #########################################
@@ -29,14 +30,41 @@ def main():
     histogram_matcher = HistogramMatcher(NEW_DATASET_OUTPUT_PATH, 'ExactHistogramMatching')
 
     # Select Reference Image
-    reference_img = imageio.imread(os.path.join('data', 'test_images', 'Fig2.tif'))
-    target_img = imageio.imread(os.path.join('data', 'test_images', 'Fig1.tif'))
+    # reference_img = imageio.imread(os.path.join('data', 'test_images', 'Fig2.tif'))
+    # target_img = imageio.imread(os.path.join('data', 'test_images', 'Fig1.tif'))
     
-    histogram_matcher.perform_histogram_matching(target_img, reference_img, display=True)
+    # histogram_matcher.perform_histogram_matching(target_img, reference_img, display=True)
 
     # Perform histogram matching
-    # histogram_matcher.perform_batch_histogram_matching(dataset, dataset[0], display=True) # TODO: dataset[0] is temporal, should we automate reference image selection?
+    # histogram_matcher.perform_batch_histogram_matching(dataset_images, dataset_images[0], display=True) # TODO: dataset[0] is temporal, should we automate reference image selection?
 
+
+    ######################################################################################################
+    ##################################### EXTRACT RADIOMICS FEATURES #####################################
+    #####################################    FROM THE NEW DATASET    #####################################
+
+    # Import the dataset
+    new_dataset = feature_extractor.import_prepare_dataset(NEW_DATASET_OUTPUT_PATH)
+
+    # Copy the segmentations from the old dataset to the new one
+    for key, value in new_dataset.items():
+        # Get the image path, replace it with the image path from the old dataset
+        # and add _roi in order to create the mask path
+        path = value['Image'].split('.')                                 # split the path into a list
+        path[0] = path[0].replace(NEW_DATASET_OUTPUT_PATH, DATASET_PATH) # replace the new path with the old one
+        path.insert(1, '_roi.')                                          # append _roi
+        path = ''.join(path)                                             # join the list elements into a string
+
+        # Add mask path from the old dataset to new dataset dictionary
+        new_dataset[key]['Mask'] = path
+
+    # Execute batch processing to extract features
+    feature_extractor.extract_features(new_dataset, NEW_FEATURES_OUTPUT_PATH)
+
+    # Get the filepaths from the images only (without the segmentations)
+    new_dataset = [value['Image'] for value in new_dataset.values()]
+
+    
     ######################################################################################################
     ########################################### COMPARE RESULTS ##########################################
 
