b2ea7759dd9c8e8ff8f1ad5990493974cfe1b93b
Geordat <58336111+Geordat@users.noreply.github.com>
2021-07-11 11:19:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,34 +8,37 @@ import os
 import glob
 
 def main():
-     ######################################################################################################
+    ######################################################################################################
   
     #====  experiments ===========================================================================
+    
     folders = os.listdir(DATASET_PATH)
     
     #onomata arxikon eikonon 
     
     pos_images_names = [glob.glob(os.path.join(DATASET_PATH, folder, "*post-contrast.tif_removed_background.png"))[0] for folder in folders]
-    
+
     flair_images_names = [glob.glob(os.path.join(DATASET_PATH, folder, "*flair.tif_removed_background.png"))[0] for folder in folders]
     
     pre_images_names = [glob.glob(os.path.join(DATASET_PATH, folder, "*pre-contrast.tif_removed_background.png"))[0] for folder in folders]
     
     all_images = [pos_images_names, flair_images_names,pre_images_names]
     
+
     
-    #arxikes eikones 
+    channel = 0 #epilogi kanalioy
     
-    images_names = all_images[1] #epilogi kanalioy 
+    images_names = all_images[channel] 
+    
     images = [ cv.imread(x, 0)  for x in images_names ]
     
 #    #ektiposi arxikon instogrammaton 
 #    
-  #  utils.plot_histograms(images,images_names)
+    #utils.plot_histograms(images,images_names)
 #    
 #    #sigrisi arxikon eikonon
 #    
-  #  print(utils.histograms_compare(images,images_names,metric=0))
+#    print(utils.histograms_compare(images,images_names,metric=0))
     
 #
 #CLAHE ===========================================================================================================
@@ -49,36 +52,45 @@ def main():
 #    print(utils.mse_compare(clahe_images,images,images_names))
    
 #histogram matching==========================================================================================
+ 
+#    ref_image = ['data/dataset/TCGA_FG_5964_20010511/TCGA_FG_5964_20010511_5_post-contrast.tif_removed_background.png',
+#                 'data/dataset/TCGA_FG_6692_20020606/TCGA_FG_6692_20020606_4_flair.tif_removed_background.png',
+#                 'data/dataset/TCGA_FG_A4MT_20020212/TCGA_FG_A4MT_20020212_5_pre-contrast.tif_removed_background.png']
 #    
-#    ref_image = 'data/dataset/TCGA_FG_5964_20010511/TCGA_FG_5964_20010511_5_post-contrast.tif_removed_background.png'
-    
-
-#        
 #    utils.plot_histograms(images,images_names)
 #
-#    hist_images = utils.histogram_matching(images,images[3] ,images_name=images_names)
+#    hist_images = utils.histogram_matching(images,cv.imread(ref_image[channel], 0) ,images_name=images_names)
 #    
 #    print(utils.histograms_compare(hist_images,images_names,metric=0))
 #    print(utils.ssim_compare(hist_images,images,images_names))
 #    
 #    print(utils.mse_compare(hist_images,images,images_names))
-#    
+    
 
 #pipeline =========================================================================================================
     
-    ref_image = images[1]
-    hist_images = utils.histogram_matching(images, ref_image,images_name=images_names)
-
+    ref_image = ['data/dataset/TCGA_FG_5964_20010511/TCGA_FG_5964_20010511_5_post-contrast.tif_removed_background.png',
+                 'data/dataset/TCGA_FG_6692_20020606/TCGA_FG_6692_20020606_4_flair.tif_removed_background.png',
+                 'data/dataset/TCGA_FG_A4MT_20020212/TCGA_FG_A4MT_20020212_5_pre-contrast.tif_removed_background.png']
+    
+                 
+    hist_images = utils.histogram_matching(images,cv.imread(ref_image[channel], 0) ,images_name=images_names)
+    
     final_images = utils.histogram_equalization_CLAHE(hist_images,tile_grid_size=(24,24), clip_limit=10,images_name=images_names)
+  
+    print(utils.histograms_compare(final_images,images_names,metric=0))
     
-#    
-#    print(utils.histograms_compare(clahe_images,images_names,metric=0))
-#    
-#    print(utils.ssim_compare(clahe_images,images,images_names))
-#    
-#    print(utils.mse_compare(clahe_images,images,images_names))
+    print(utils.ssim_compare(final_images,images,images_names))
+    
+    print(utils.mse_compare(final_images,images,images_names))
     
 
+  
+#    text = '/CLAHE_CLIP_5'
+#    for i in range(0,len(images)):
+#        if not os.path.exists('images/'+pos_images_names[i][-64:-27]):
+#            os.makedirs('images/'+pos_images_names[i][-64:-27])
+#            cv.imwrite('images/'+pos_images_names[i][-64:-27]+text+'.png',images[i]) 
 
     ######################################################################################################
     ##################################### EXTRACT RADIOMICS FEATURES #####################################
